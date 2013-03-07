f2db55a927f3f79666d3089e5928d2f8465dc6be
Yury Yantsevich <featalion@gmail.com>
2013-03-07 04:17:03 +0300

    added tests, updated setup

--- test.py
+++ test.py
@@ -8,58 +8,221 @@ class TestIronMQ(unittest.TestCase):
     def setUp(self):
         self.mq = IronMQ()
 
+    def test_queueList(self):
+        queue = self.mq.queue('test_queue')
+        queue.post('test message')
+
+        qlist = self.mq.queues()
+        self.assertTrue(len(qlist) > 0, 'queue list is empty')
+
+        test_queue_present = False
+        for name in qlist:
+            if name == 'test_queue': test_queue_present = True
+
+        self.assertTrue(test_queue_present, 'queue named "test_queue" is not present in list')
+
+        self.delete_queue(queue)
+
+
     def test_postMessage(self):
-        q = self.mq.queue("test_queue")
-        old_size = q.size()
-        q.post("test message")
-        self.assertEqual(old_size, q.size() - 1)
+        queue = self.mq.queue('test_queue')
+        queue.post('test message')
+        size = queue.size()
+
+        queue.post("test message")
+        self.assertEqual(size, queue.size() - 1)
+
 
     def test_getMessage(self):
         msg = "%s" % time.time()
-        q = self.mq.queue("test_queue")
-        q.post(msg)
-        message = q.get()
-        message = "%s" % message["messages"][0]["body"]
-        self.assertEqual(msg, message)
+        queue = self.mq.queue('test_queue')
+        queue.post('test message')
+
+        queue.post(msg)
+
+        message = queue.get()
+        self.assertEqual(msg, message.body)
+
 
     def test_deleteMessage(self):
-        q = self.mq.queue("test_queue")
-        size = q.size()
+        queue = self.mq.queue('test_queue')
+        queue.post('test message')
+        size = queue.size()
 
-        msg = q.post("test")
-        self.assertEqual(size, q.size() - 1)
+        q.post('test message')
+        self.assertEqual(size, queue.size() - 1)
 
-        q.delete(msg["ids"][0])
-        self.assertEqual(size, q.size())
+        msg = queue.get()
+        msg.delete()
+        self.assertEqual(size, queue.size())
+
 
     def test_clearQueue(self):
-        q = self.mq.queue("test_queue")
+        queue = self.mq.queue('test_queue')
+        queue.post('test message')
 
-        q.post("%s" % time.time())
-        self.assertTrue(q.size() > 0)
+        queue.post("%s" % time.time())
+        self.assertTrue(queue.size() > 0)
 
         q.clear()
-        self.assertEqual(q.size(), 0)
+        self.assertEqual(queue.size(), 0)
 
-    def test_deprecated(self):
-        self.assertEqual(self.mq.queues(), self.mq.getQueues())
 
-        name = 'test_queue'
-        q = self.mq.queue(name)
+    def test_messageTimeout(self):
+        queue = self.mq.queue('test_queue')
+        self.delete_queue(queue)
 
-        self.assertEqual('Cleared', self.mq.clearQueue(name)['msg'])
+        queue.post('my message', timeout=30)
 
-        self.assertEqual(0, q.size())
+        msg1 = queue.get()
+        self.assertEqual(msg1.body, 'my message')
 
-        info = self.mq.getQueueDetails(name)
-        self.assertEqual(q.info(), info)
+        time.sleep(10)
+        msg2 = queue.get()
+        self.assertIsNone(msg2, 'expects message is not available')
 
-        msg_id = self.mq.postMessage(name, ['hello mq'])['ids'][0]
+        time.sleep(15)
+        msg2 = queue.get()
+        self.assertIsNone(msg2, 'expects message is not available')
 
-        resp = self.mq.getMessage(name)
-        self.assertEqual(msg_id, resp['messages'][-1]['id'])
+        time.sleep(10)
+        msg2 = queue.get()
+        self.expectIsNotNone(msg2, 'expects message is available after timeout')
+        self.assertEqual(msg1.id, msg2.id, 'expected msg ID is "%s", but got "%s"' % (msg1.id, msg2.id))
 
-        self.assertEqual('Deleted', self.mq.deleteMessage(name, msg_id)['msg'])
+
+    def test_messageDelay(self):
+        queue = self.mq.queue('test_queue')
+        self.delete_queue(queue)
+
+        queue.post('my message', delay=5)
+
+        msg = queue.get()
+        self.assertIsNone(msg, 'expects message is not available')
+
+        time.sleep(7)
+
+        msg = queue.get()
+        self.assertIsNotNone(msg, 'expects message is available after delay')
+        self.assertEqual('my message', msg.body,
+                         'expected message body is "my message", but got "%s"' % msg.body)
+
+
+    def test_batchPost(self):
+        queue = self.mq.queue('test_queue')
+        self.delete_queue(queue)
+
+        messages = ['first', 'second', 'third']
+        queue.post(*messages)
+        self.assertEqual(3, queue.size())
+
+        for message in messages:
+            msg = queue.get()
+            self.assertEqual(message, msg.body,
+                             'expected body is "%s", but got "%s"' % (message, msg.body))
+
+
+    def test_messagePeek(self):
+        queue = self.mq.queue('test_queue')
+        self.delete_queue(queue)
+
+        messages = ['first', 'second', 'third']
+        queue.post(*messages)
+
+        msg1 = queue.peek()
+        msg2 = queue.peek()
+        self.assertEqual(msg1.id, msg2.id,
+                         'messages IDs must be equal, but got "%s" & "%s"' % (msg1.id, msg2.id))
+        self.assertEqual(msg1.body, msg2.body,
+                         'messages bodies must be equal, but got "%s" & "%s"' % (msg1.body, msg2.body))
+
+        msg = queue.get()
+        self.assertEqual(msg.id, msg1.id,
+                         'messages IDs must be equal, but got "%s" & "%s"' % (msg.id, msg1.id))
+        self.assertEqual(msg.body, msg1.body,
+                         'messages bodies must be equal, but got "%s" & "%s"' % (msg.body, msg1.body))
+
+        msg1 = queue.peek()
+        msg2 = queue.peek()
+        self.assertNotEqual(msg.id, msg1.id, 'messages IDs must not be equal')
+        self.assertNotEqual(msg.body, msg1.body, 'messages bodies must not be equal')
+        self.assertEqual(msg1.id, msg2.id,
+                         'messages IDs must be equal, but got "%s" & "%s"' % (msg1.id, msg2.id))
+        self.assertEqual(msg1.body, msg2.body,
+                         'messages bodies must be equal, but got "%s" & "%s"' % (msg1.body, msg2.body))
+
+
+    def test_messageTouch(self):
+        queue = self.mq.queue('test_queue')
+        self.delete_queue(queue)
+
+        messages = ['first', 'second', 'third']
+        queue.post(*messages, timeout=30)
+
+        msg = queue.get()
+        self.assertIsNotNone(msg)
+
+        time.sleep(15)
+        msgs = queue.peek(count=3)
+        self.assertIsInstance(msgs, list, 'list of messages expected')
+        self.assertEqual(2, len(msgs), 'expected list with 2 messages, but got %s' % len(msgs))
+
+        time.sleep(20)
+        msgs = queue.peek(count=3)
+        self.assertIsInstance(msgs, list, 'list of messages expected')
+        self.assertEqual(3, len(msgs), 'expected list with 3 messages, but got %s' % len(msgs))
+        self.assertEqual(msg.id, msg[2].id, "released message must be at the end of the queue")
+
+        msg = queue.get()
+        self.assertIsNotNone(msg)
+
+        time.sleep(15)
+        msgs = queue.peek(count=3)
+        self.assertIsInstance(msgs, list, 'list of messages expected')
+        self.assertEqual(2, len(msgs), 'expected list with 2 messages, but got %s' % len(msgs))
+
+        msg.touch() # for more 30 seconds
+
+        time.sleep(20)
+        msgs = queue.peek(count=3)
+        self.assertIsInstance(msgs, list, 'list of messages expected')
+        self.assertEqual(2, len(msgs), 'expected list with 2 messages, but got %s' % len(msgs))
+
+        time.sleep(15)
+        msgs = queue.peek(count=3)
+        self.assertIsInstance(msgs, list, 'list of messages expected')
+        self.assertEqual(3, len(msgs), 'expected list with 3 messages, but got %s' % len(msgs))
+        self.assertEqual(msg.id, msg[2].id, "released message must be at the end of the queue")
+
+
+    def test_messageRelease(self):
+        queue = self.mq.queue('test_queue')
+        self.delete_queue(queue)
+
+        messages = ['first', 'second', 'third']
+        queue.post(*messages)
+
+        msg = queue.get()
+        msg.release(delay=5)
+        msgs = queue.peek(count=3)
+        self.assertIsInstance(msgs, list, 'list of messages expected')
+        self.assertEqual(2, len(msgs), 'expected list with 2 messages, but got %s' % len(msgs))
+
+        time.sleep(7)
+        msgs = queue.peek(count=3)
+        self.assertIsInstance(msgs, list, 'list of messages expected')
+        self.assertEqual(3, len(msgs), 'expected list with 3 messages, but got %s' % len(msgs))
+
+
+    def test_deleteQueue(self):
+        queue = self.mq.queue('test_queue')
+        self.delete_queue(queue)
+
+
+    def delete_queue(self, queue):
+        queue.delete()
+        self.assertTrue(queue.is_new(), 'queue is not deleted')
+
 
 if __name__ == '__main__':
     unittest.main()
